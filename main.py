337b26e2c923000b2da0f51e9d0f2506d8fa70bc
Pri-0526 <pitygiuliano26@gmail.com>
2025-04-30 11:59:03 +0000

    true or false

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ print(variable)#none
 numero = 3.1416 # numero real
 pi = -numero 
 print(pi)
+Variable=1
+gabriel="gabriel.sanchez@bue.edu.ar"
+comparacion = variable ==1
+print (comparacion)
+comparacion = not comparacion
+print (comparacion)
+mayor = variable > 0
+print (mayor)
+menor = variable < 0
+mayor = (1+1) > 1
+menor = (20-30) < 10
+y = mayor and menor 
+print (y, "and")
+
