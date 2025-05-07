0ea6c89c7d7434cca74f5dca60fab0ab6f4934fb
Pri-0526 <pitygiuliano26@gmail.com>
2025-05-07 11:52:12 +0000

    problema

---
+++ programa.py
@@ -0,0 +1,12 @@
+variable1 = 3
+variable2 = 6
+variable1 + variable2
+print (variable1 + variable2)
+menor = variable1 < variable2
+
+mayor = variable1 > variable2
+print ("3 es mayor que 6?", mayor)
+print ("3 es menor que 6?", menor)
+
+comparacion = variable1 == variable2
+print ("3 es igual a 6?", comparacion)
