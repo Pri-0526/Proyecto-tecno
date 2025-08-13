75c49b2c128d80a62ab72e0ca2e507217c790b50
Pri-0526 <pitygiuliano26@gmail.com>
2025-08-13 11:51:30 +0000

    herenciadeclases

---
+++ clase4/13-8.py
@@ -0,0 +1,34 @@
+class Persona:
+    def hablar (self):
+        pass
+class Padre (Persona):
+    apellido= " "
+    def __init__(self,nombre):
+        self.nombre=nombre
+    def __str__(self):
+        return self.nombre + " " +self.apellido
+    def hablar (self):
+        print ("hola soy", self.nombre, self.apellido)
+
+class Hijo (Padre): 
+    def __init__(self,nombre,losDeportes):
+        super().__init__(nombre)
+        self.losDeportes=losDeportes
+    def hablar (self):
+        print ("hola soy",self.nombre,self.apellido, "y me gusta", self.losDeportes)
+    
+class Hija (Padre): 
+    def __init__(self,nombre,Musica):
+        super().__init__(nombre)
+        self.Musica=Musica
+    def hablar (self):
+        print ("hola soy",self.nombre,self.apellido, "y me gusta", self.Musica)
+
+
+padre = Padre("nombre del padre")
+Padre.apellido= "apellido del padre"
+hijo = Hijo ("nombre del hijo" , "python")
+hijo.hablar ()
+hija = Hija("nombre del hija" , "nombre de banda")
+hija.hablar ()
+
